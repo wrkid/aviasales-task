083d585e0f6e2afab64a41794b29317f23b3a98f
wrkid <137333242+wrkid@users.noreply.github.com>
2023-11-01 18:17:02 +0300

    layout tabs w

--- src/components/Tabs/tabs.jsx
+++ src/components/Tabs/tabs.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './tabs.scss'
 
 export default function Tabs() {
+  const [ activeTab, setActiveTab ] = useState('tab1');
+
   return (
     <div className="Tabs">
       {/* Tab nav */}
       <ul className="nav">
-        <li>САМЫЙ ДЕШЕВЫЙ</li>
-        <li>САМЫЙ БЫСТРЫЙ</li>
-        <li>ОПТИМАЛЬНЫЙ</li>
+        <li className={activeTab === 'tab1' ? 'active' : ''}
+          onClick={() => setActiveTab('tab1')}>САМЫЙ ДЕШЕВЫЙ</li>
+        <li className={activeTab === 'tab2' ? 'active' : ''}
+          onClick={() => setActiveTab('tab2')}>САМЫЙ БЫСТРЫЙ</li>
+        <li className={activeTab === 'tab3' ? 'active' : ''}
+          onClick={() => setActiveTab('tab3')}>ОПТИМАЛЬНЫЙ</li>
       </ul>
       <div className="outlet">
         {/* content will be shown here */}
